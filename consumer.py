3833b5cd36251123ed029f30f45125631c386829
ddotu2612 <dovantu29012000@gmail.com>
2023-02-22 15:38:35 +0700

    tudv upcode

--- consumer.py
+++ consumer.py
@@ -1,22 +1,31 @@
-# Import KafkaConsumer from Kafka library
-from kafka import KafkaConsumer
+# # Import KafkaConsumer from Kafka library
+# from kafka import KafkaConsumer
 
-# Import sys module
-import sys
+# # Import sys module
+# import sys
 
-# Define server with port
-bootstrap_servers = ['localhost:29092']
+# # Define server with port
+# bootstrap_servers = ['localhost:29092']
 
-# Define topic name from where the message will recieve
-topicName = 'demo1'
+# # Define topic name from where the message will recieve
+# topicName = 'demo1'
 
-# Initialize consumer variable
-consumer = KafkaConsumer (topicName, bootstrap_servers =
-   bootstrap_servers)
+# # Initialize consumer variable
+# consumer = KafkaConsumer (topicName, bootstrap_servers =
+#    bootstrap_servers)
 
-# Read and print message from consumer
-for msg in consumer:
-    print("Topic Name=%s,Message=%s"%(msg.topic,msg.value))
+# # Read and print message from consumer
+# for msg in consumer:
+#     print("Topic Name=%s,Message=%s"%(msg.topic,msg.value))
 
-# Terminate the script
-sys.exit()
+# # Terminate the script
+# sys.exit()
+
+
+import json
+
+f = open('json.txt')
+
+data = json.load(f)
+
+print(data)
